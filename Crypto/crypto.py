6963b9b25e6fb17c0492d9c0a54ec2f86fbb9486
Sᴏᴜᴍᴏ <134998475+ImSoumo@users.noreply.github.com>
2024-08-12 19:16:32 +0530

    completed

---
+++ Crypto/crypto.py
@@ -0,0 +1,39 @@
+import requests
+
+async def check_transaction_ethereum(TxId):
+    ERC_API_KEY = "QHTC1CRQUJF743SHY1UDZ82HTJGS74ENB4"
+    check = f"https://api.etherscan.io/api?module=transaction&action=gettxreceiptstatus&txhash={TxId}&apikey={ERC_API_KEY}"
+    response = requests.get(check).json()
+    return response
+
+async def check_transaction_tron(TxId):
+    check = f"https://api.trongrid.io/v1/transaction/{TxId}"
+    response = requests.get(check).json()
+    return response
+
+async def check_transaction_bsc(TxId):
+    BNB_API_KEY = "3AIXQKWXE9FH4TZJAJEDNQK2VRCBJ4UMV1"
+    check = f"https://api.bscscan.io/api?module=transaction&action=gettxreceiptstatus&txhash={TxId}&apikey={BNB_API_KEY}"
+    response = requests.get(check).json()
+    return response
+
+async def format_ethereum_response(response):
+    if response['status'] == '1':
+        details = f"Transaction Status: Success\nTransaction Details: {response['result']}"
+    else:
+        details = f"Transaction Status: Fail\nTransaction Details: {response['result']}"
+    return details
+
+async def format_tron_response(response):
+    if response['success']:
+        details = f"Transaction Status: Success\nTransaction Details: {response['data']}"
+    else:
+        details = f"Transaction Status: Fail\nTransaction Details: {response['data']}"
+    return details
+
+async def format_bsc_response(response):
+    if response['status'] == '1':
+        details = f"Transaction Status: Success\nTransaction Details: {response['result']}"
+    else:
+        details = f"Transaction Status: Fail\nTransaction Details: {response['result']}"
+    return details
